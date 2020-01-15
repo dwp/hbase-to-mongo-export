614d53beb3aa28a15a691ac5b1d98892f07117f7
steveburton4 <Steven.burton@hotmail.co.uk>
2020-01-15 13:41:05 +0000

    Add totals to logs (#76)
    * Add totals to logs
    
    * Add totals to logs

--- src/main/kotlin/app/batch/StreamingManifestWriter.kt
+++ src/main/kotlin/app/batch/StreamingManifestWriter.kt
@@ -15,19 +15,30 @@ import java.io.FileInputStream
 @Component
 class StreamingManifestWriter {
 
-    fun sendManifest(s3: AmazonS3, manifestFile: File, manifestBucket: String, manifestPrefix: String) {
+    fun sendManifest(s3: AmazonS3, manifestFile: File, manifestBucket: String, manifestPrefix: String) : Boolean {
         try {
             val manifestSize = manifestFile.length()
             val manifestFileName = manifestFile.name
             val manifestFileMetadata = manifestMetadata(manifestFileName, manifestSize)
             val prefix = "$manifestPrefix/$manifestFileName"
-            logInfo(logger, "Writing manifest manifestFile to s3", "s3_location", "s3://$manifestBucket/$manifestPrefix/$manifestFileName", "manifest_size", "$manifestSize")
+
+            logInfo(logger, "Writing manifest manifestFile to s3", 
+                "s3_location", "s3://$manifestBucket/$manifestPrefix/$manifestFileName", 
+                "manifest_size", "$manifestSize", 
+                "total_manifest_files_already_written", "$totalManifestFiles",
+                "total_manifest_records_already_written", "$totalManifestRecords")
+
             BufferedInputStream(FileInputStream(manifestFile)).use { inputStream ->
                 val request = PutObjectRequest(manifestBucket, prefix, inputStream, manifestFileMetadata)
                 s3.putObject(request)
             }
+
+            totalManifestFiles++
+            totalManifestRecords += manifestSize
+            return true
         } catch (e: Exception) {
             logError(logger, "Failed to write manifest", e, "manifest_file", "$manifestFile")
+            return false
         }
     }
 
@@ -41,5 +52,7 @@ class StreamingManifestWriter {
     companion object {
         val logger: Logger = LoggerFactory.getLogger(StreamingManifestWriter::class.toString())
     }
-
+    
+    private var totalManifestFiles = 0
+    private var totalManifestRecords : Long = 0
 }
