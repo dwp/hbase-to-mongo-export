a586c663c463415b4fa6986c28b0ccbbd0faed10
steveburton4 <Steven.burton@hotmail.co.uk>
2020-01-15 20:29:42 +0000

    Add synchronised command (#78)
    * Add synchronised command
    
    * Static for start time
    
    * Static for start time
    
    * Static for start time

--- src/main/kotlin/app/utils/logging/LoggerUtils.kt
+++ src/main/kotlin/app/utils/logging/LoggerUtils.kt
@@ -34,8 +34,13 @@ private var application = System.getProperty("application", UNSET_TEXT)
 private var app_version = System.getProperty("app_version", UNSET_TEXT)
 private var component = System.getProperty("component", UNSET_TEXT)
 private var correlation_id = System.getProperty("correlation_id", UNSET_TEXT)
-private var start_time_milliseconds = System.getProperty("start_time_milliseconds", UNSET_TEXT)
 private var staticData = makeLoggerStaticDataTuples()
+
+class LogConfiguration {
+    companion object {
+        var start_time_milliseconds = System.currentTimeMillis()
+    }
+}
 
 fun makeUtcDateFormat(): SimpleDateFormat {
     // 2001-07-04T12:08:56.235
@@ -62,7 +67,6 @@ fun resetLoggerStaticFieldsForTests() {
     app_version = System.getProperty("app_version", UNSET_TEXT)
     component = System.getProperty("component", UNSET_TEXT)
     correlation_id = System.getProperty("correlation_id", UNSET_TEXT)
-    start_time_milliseconds = System.getProperty("start_time_milliseconds", UNSET_TEXT)
     staticData = makeLoggerStaticDataTuples()
 }
 
@@ -73,7 +77,7 @@ fun overrideLoggerStaticFieldsForTests(topic: String, host: String, env: String,
     application = app
     app_version = version
     component = comp
-    start_time_milliseconds = start_milliseconds
+    LogConfiguration.start_time_milliseconds = start_milliseconds.toLong()
     correlation_id = id
     staticData = makeLoggerStaticDataTuples()
 }
@@ -175,11 +179,7 @@ fun throwableProxyEventToString(event: ILoggingEvent): String {
 }
 
 fun getDurationInMilliseconds(epochTime: Long): String {
-    if (start_time_milliseconds == UNSET_TEXT) {
-        start_time_milliseconds = System.getProperty("start_time_milliseconds", UNSET_TEXT)
-    }
-
-    var elapsed_milliseconds = epochTime - start_time_milliseconds.toLong()
+    var elapsed_milliseconds = epochTime - LogConfiguration.start_time_milliseconds
     return elapsed_milliseconds.toString()
 }
 
