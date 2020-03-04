33c9504abca2ccbd2af794ec4306f6f388ac2c84
Daniel Chicot <dan.chicot@gmail.com>
2020-03-04 14:38:54 +0000

    Minor change to kick off build.

--- src/test/kotlin/app/batch/ValidatorTest.kt
+++ src/test/kotlin/app/batch/ValidatorTest.kt
@@ -23,9 +23,7 @@ class ValidatorTest {
         val decryptedDbObject = """{"_id": {"someId": "RANDOM_GUID", "declarationId": 1234}, "type": "addressDeclaration", "contractId": 1234, "addressNumber": {"type": "AddressLine", "cryptoId": 1234}, "addressLine2": null, "townCity": {"type": "AddressLine", "cryptoId": 1234}, "postcode": "SM5 2LE", "processId": 1234, "effectiveDate": {"type": "SPECIFIC_EFFECTIVE_DATE", "date": 20150320, "knownDate": 20150320}, "paymentEffectiveDate": {"type": "SPECIFIC_EFFECTIVE_DATE", "date": 20150320, "knownDate": 20150320}, "createdDateTime": {"${"$"}date": "2015-03-20T12:23:25.183Z", "_archivedDateTime": "should be replaced by _archivedDateTime"}, "_version": 2, "_archived": "should be replaced by _removed", "unicodeNull": "\u0000", "unicodeNullwithText": "some\u0000text", "lineFeedChar": "\n", "lineFeedCharWithText": "some\ntext", "carriageReturn": "\r", "carriageReturnWithText": "some\rtext", "carriageReturnLineFeed": "\r\n", "carriageReturnLineFeedWithText": "some\r\ntext", "_lastModifiedDateTime": "2018-12-14T15:01:02.000+0000"}"""
         val lastModified = "2019-07-04T07:27:35.104+0000"
         val encryptionBlock: EncryptionBlock =
-            EncryptionBlock("keyEncryptionKeyId",
-                "initialisationVector",
-                "encryptedEncryptionKey")
+            EncryptionBlock("keyEncryptionKeyId","initialisationVector","encryptedEncryptionKey")
         val sourceRecord = SourceRecord(generateFourByteChecksum("00001"),
                 10, encryptionBlock, "dbObject", "db", "collection", lastModified)
         val decrypted = validator.skipBadDecryptedRecords(sourceRecord, decryptedDbObject)
