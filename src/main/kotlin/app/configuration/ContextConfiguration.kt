c42b2f376a5ecb71c34a268a48838298f9f07e06
Daniel Chicot <dan.chicot@gmail.com>
2020-03-06 15:37:56 +0000

    Default timeout is 24 hrs.

--- src/main/kotlin/app/configuration/ContextConfiguration.kt
+++ src/main/kotlin/app/configuration/ContextConfiguration.kt
@@ -102,10 +102,7 @@ class ContextConfiguration {
         val configuration = HBaseConfiguration.create().apply {
             set("hbase.zookeeper.quorum", hbaseZookeeperQuorum)
             setInt("hbase.zookeeper.port", 2181)
-            if (hbaseTimeout.toInt() > 0) {
-                setInt("hbase.client.scanner.timeout.period", if (hbaseTimeout.toInt() > 0) hbaseTimeout.toInt() else 24 * 60 * 60 * 1000)
-            }
-
+            setInt("hbase.client.scanner.timeout.period", if (hbaseTimeout.toInt() > 0) hbaseTimeout.toInt() else 24 * 60 * 60 * 1000)
         }
 
         val timeout = configuration.get("hbase.client.scanner.timeout.period")
