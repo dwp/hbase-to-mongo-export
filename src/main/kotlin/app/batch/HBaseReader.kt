6192944fab0d1fc3ff49a900970da62f3d9c5bcd
Steven Burton <stevenburton@digital.uc.dwp.gov.uk>
2020-04-02 20:36:28 +0100

    Fix entry

--- src/main/kotlin/app/batch/HBaseReader.kt
+++ src/main/kotlin/app/batch/HBaseReader.kt
@@ -137,8 +137,8 @@ class HBaseReader constructor(private val connection: Connection, private val te
 
     fun getScanTimeRangeEndEpoch() : Long {
         var endDateTime = ZonedDateTime.now()
-        if (scanTimeRangeStart != "") {
-            endDateTime = ZonedDateTime.parse(scanTimeRangeStart)
+        if (scanTimeRangeEnd != "") {
+            endDateTime = ZonedDateTime.parse(scanTimeRangeEnd)
         }
 
         return endDateTime.toInstant().toEpochMilli();
