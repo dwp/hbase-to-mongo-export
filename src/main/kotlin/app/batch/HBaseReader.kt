84a9c0ffe5e92117f09eae6f8df5e825bb570161
Daniel Chicot <dan.chicot@gmail.com>
2020-04-20 18:31:43 +0100

    lang -> lang3

--- src/main/kotlin/app/batch/HBaseReader.kt
+++ src/main/kotlin/app/batch/HBaseReader.kt
@@ -8,7 +8,7 @@ import app.utils.logging.logError
 import app.utils.logging.logInfo
 import com.google.gson.Gson
 import com.google.gson.JsonObject
-import org.apache.commons.lang.StringUtils
+import org.apache.commons.lang3.StringUtils
 import org.apache.hadoop.hbase.TableName
 import org.apache.hadoop.hbase.client.Connection
 import org.apache.hadoop.hbase.client.ResultScanner
