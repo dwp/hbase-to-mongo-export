7cd7a19ad06ec9e24fe1db8456f2f4872f9865ad
Daniel Chicot <dan.chicot@gmail.com>
2020-07-17 15:58:49 +0100

    Added, committed unit test class.

---
+++ src/test/kotlin/app/batch/HBaseReaderTest.kt
@@ -0,0 +1,161 @@
+package app.batch
+
+import app.exceptions.ScanRetriesExhaustedException
+import app.utils.TextUtils
+import com.nhaarman.mockitokotlin2.*
+import org.apache.hadoop.hbase.NotServingRegionException
+import org.apache.hadoop.hbase.TableName
+import org.apache.hadoop.hbase.client.Connection
+import org.apache.hadoop.hbase.client.Result
+import org.apache.hadoop.hbase.client.ResultScanner
+import org.apache.hadoop.hbase.client.Scan
+import org.apache.hadoop.hbase.client.Table
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.springframework.test.util.ReflectionTestUtils
+
+class HBaseReaderTest {
+
+    @Test
+    fun onSuccessfulScanDoesNotRetry() {
+        val result = mock<Result> {
+            on { row } doReturn byteArrayOf(2)
+        }
+
+        val resultScanner = mock<ResultScanner>() {
+            on { next() } doReturn result
+        }
+
+        val table = mock<Table> {
+            on { getScanner(any<Scan>()) } doReturn resultScanner
+        }
+
+        val tableName = "database:collection"
+        val textUtils = TextUtils()
+        val connection = mock<Connection> {
+            on { getTable(TableName.valueOf(tableName)) } doReturn table
+        }
+
+        val hBaseReader = HBaseReader(connection, textUtils)
+        ReflectionTestUtils.setField(hBaseReader, "scanRetrySleepMs", "1")
+        ReflectionTestUtils.setField(hBaseReader, "scanMaxRetries", "5")
+        ReflectionTestUtils.setField(hBaseReader, "topicName", "db.database.collection")
+        ReflectionTestUtils.setField(hBaseReader, "start", 0)
+        ReflectionTestUtils.setField(hBaseReader, "stop", 10)
+        val spy = spy(hBaseReader)
+        var actual = spy.read()
+        verify(spy, times(1)).read()
+        verify(connection, times(1)).getTable(TableName.valueOf(tableName))
+        val argumentCaptor = argumentCaptor<Scan>()
+        verify(table, times(1)).getScanner(argumentCaptor.capture())
+        val scan = argumentCaptor.firstValue
+        assertArrayEquals(byteArrayOf(0), scan.startRow)
+        assertArrayEquals(byteArrayOf(10), scan.stopRow)
+        assertArrayEquals(byteArrayOf(2), result.row)
+        assertEquals(result, actual)
+    }
+
+    @Test
+    fun onUnsuccessfulScanDoesRetry() {
+        val firstResult = mock<Result> {
+            on { row } doReturn byteArrayOf(2)
+        }
+
+        val failingScanner = mock<ResultScanner>() {
+            on { next() } doReturn firstResult doThrow NotServingRegionException("Error")
+        }
+
+        val secondResult = mock<Result> {
+            on { row } doReturn byteArrayOf(5)
+        }
+
+        val successfulScanner = mock<ResultScanner>() {
+            on { next() } doReturn secondResult doReturn null
+        }
+
+        val table = mock<Table> {
+            on { getScanner(any<Scan>()) } doReturn failingScanner doReturn successfulScanner
+        }
+
+        val tableName = "database:collection"
+        val textUtils = TextUtils()
+        val connection = mock<Connection> {
+            on { getTable(TableName.valueOf(tableName)) } doReturn table
+        }
+
+        val hBaseReader = HBaseReader(connection, textUtils)
+        ReflectionTestUtils.setField(hBaseReader, "scanRetrySleepMs", "1")
+        ReflectionTestUtils.setField(hBaseReader, "scanMaxRetries", "5")
+        ReflectionTestUtils.setField(hBaseReader, "topicName", "db.database.collection")
+        ReflectionTestUtils.setField(hBaseReader, "start", 0)
+        ReflectionTestUtils.setField(hBaseReader, "stop", 10)
+
+        val spy = spy(hBaseReader)
+        var actualFirstResult = spy.read()
+        var actualSecondResult = spy.read()
+        var actualThirdResult = spy.read()
+        val argumentCaptor = argumentCaptor<Scan>()
+        verify(table, times(2)).getScanner(argumentCaptor.capture())
+        val firstScan = argumentCaptor.firstValue
+        val secondScan = argumentCaptor.secondValue
+
+        assertArrayEquals(byteArrayOf(0), firstScan.startRow)
+        assertArrayEquals(byteArrayOf(2), secondScan.startRow)
+        assertNotNull(actualFirstResult)
+        assertNotNull(actualSecondResult)
+        assertNull(actualThirdResult)
+        assertArrayEquals(byteArrayOf(2), actualFirstResult!!.row)
+        assertArrayEquals(byteArrayOf(5), actualSecondResult!!.row)
+    }
+
+    @Test(expected = ScanRetriesExhaustedException::class)
+    fun onUnsuccessfulScanGivesUpAfterMaxRetries() {
+        val firstResult = mock<Result> {
+            on { row } doReturn byteArrayOf(3)
+        }
+
+        val firstFailingScanner = mock<ResultScanner>() {
+            on { next() } doReturn firstResult doThrow NotServingRegionException("Error")
+        }
+
+
+        val secondFailingScanner = mock<ResultScanner>() {
+            on { next() } doThrow NotServingRegionException("Error")
+        }
+
+        val table = mock<Table> {
+            on { getScanner(any<Scan>()) } doReturn firstFailingScanner doReturn secondFailingScanner
+        }
+
+        val tableName = "database:collection"
+        val textUtils = TextUtils()
+        val connection = mock<Connection> {
+            on { getTable(TableName.valueOf(tableName)) } doReturn table
+        }
+
+        val hBaseReader = HBaseReader(connection, textUtils)
+        ReflectionTestUtils.setField(hBaseReader, "scanRetrySleepMs", "1")
+        ReflectionTestUtils.setField(hBaseReader, "scanMaxRetries", "5")
+        ReflectionTestUtils.setField(hBaseReader, "topicName", "db.database.collection")
+        ReflectionTestUtils.setField(hBaseReader, "start", 0)
+        ReflectionTestUtils.setField(hBaseReader, "stop", 10)
+        try {
+            val spy = spy(hBaseReader)
+            while (true) {
+                spy.read()
+            }
+            fail("Should have thrown exception")
+        }
+        finally {
+            val argumentCaptor = argumentCaptor<Scan>()
+            verify(table, times(5)).getScanner(argumentCaptor.capture())
+            val firstArg = argumentCaptor.firstValue
+            val subsequentArgs = argumentCaptor.allValues.drop(1)
+            assertEquals(4, subsequentArgs.size)
+            subsequentArgs.forEach {
+                assertArrayEquals(byteArrayOf(3), it.startRow)
+            }
+        }
+    }
+}
