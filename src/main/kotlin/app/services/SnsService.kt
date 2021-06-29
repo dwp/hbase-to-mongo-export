cb7a4ffb60a8beae8a12c062fd87c7aba4f391da
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-29 11:56:26 +0100

    Fix SNS service

--- src/main/kotlin/app/services/SnsService.kt
+++ src/main/kotlin/app/services/SnsService.kt
@@ -2,5 +2,5 @@ package app.services
 
 interface SnsService {
     fun sendExportCompletedSuccessfullyMessage()
-    fun sendMonitoringMessage(completionStatus: ExportCompletionStatus)
+    fun sendMonitoringMessage(exitStatus: ExitStatus
 }
