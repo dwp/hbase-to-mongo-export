c8fb7c1948ac43afafba838a83031748892e6f79
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-10-05 11:49:16 +0100

    -
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- src/main/kotlin/app/batch/JobCompletionNotificationListener.kt
+++ src/main/kotlin/app/batch/JobCompletionNotificationListener.kt
@@ -103,7 +103,7 @@ class JobCompletionNotificationListener(
 
 
     private fun sendRisJsons(completionStatus: ExportCompletionStatus) {
-        if (completionStatus.equals(ExportCompletionStatus.COMPLETED_SUCCESSFULLY) && sendToRis.toBoolean()) {
+        if (completionStatus.equals(ExportCompletionStatus.COMPLETED_SUCCESSFULLY) || completionStatus.equals(ExportCompletionStatus.COMPLETED_UNSUCCESSFULLY)) {
             messagingService.sendDataEgressMessage(pdmCommonModelSitePrefix)
         }
     }
