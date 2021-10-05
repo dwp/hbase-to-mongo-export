27129d4ad32dd91a3a275ed933600cc4a4c437a5
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-10-05 08:47:37 +0100

    remove a verify test
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- src/test/kotlin/app/batch/JobCompletionNotificationListenerTest.kt
+++ src/test/kotlin/app/batch/JobCompletionNotificationListenerTest.kt
@@ -135,7 +135,7 @@ class JobCompletionNotificationListenerTest {
             }
             jobCompletionNotificationListener.afterJob(jobExecution)
             verify(messagingService, times(1)).notifySnapshotSenderNoFilesExported()
-            verify(messagingService, times(1)).sendDataEgressMessage(TEST_PDM_COMMON_MODEL_INPUTS_PREFIX)
+//            verify(messagingService, times(1)).sendDataEgressMessage(TEST_PDM_COMMON_MODEL_INPUTS_PREFIX)
             verifyNoMoreInteractions(messagingService)
             verify(pushgatewayService, times(1)).pushFinalMetrics()
             verifyNoMoreInteractions(pushgatewayService)
