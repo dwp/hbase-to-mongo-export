9f6c906296fb3a7b835e69ee5c2c0038da1b9077
Levan Kirvalidze <levanik@live.com>
2021-10-07 12:55:54 +0100

    2 invocations
    Signed-off-by: Levan Kirvalidze <levanik@live.com>

--- src/test/kotlin/app/batch/JobCompletionNotificationListenerTest.kt
+++ src/test/kotlin/app/batch/JobCompletionNotificationListenerTest.kt
@@ -133,6 +133,7 @@ class JobCompletionNotificationListenerTest {
             }
             jobCompletionNotificationListener.afterJob(jobExecution)
             verify(messagingService, times(1)).notifySnapshotSenderNoFilesExported()
+            verify(messagingService, times(2)).sendDataEgressMessage(TEST_PDM_COMMON_MODEL_INPUTS_PREFIX)
             verifyNoMoreInteractions(messagingService)
             verify(pushgatewayService, times(1)).pushFinalMetrics()
             verifyNoMoreInteractions(pushgatewayService)
