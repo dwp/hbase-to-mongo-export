088e3925d0b48dcb4dbf20f2c5ce556943e71221
Daniel Chicot <dan.chicot@gmail.com>
2021-10-11 15:43:55 +0100

    Log scan id. (#216)
    * Log scan id.
    
    Log ids of all records processed during an incremental export.
    
    * Added a comment regarding which exports ids should not be logged (i.e. the full export).
    
    * Typo.

--- src/main/kotlin/app/batch/HBaseResultProcessor.kt
+++ src/main/kotlin/app/batch/HBaseResultProcessor.kt
@@ -49,7 +49,11 @@ class HBaseResultProcessor(private val textUtils: TextUtils): ItemProcessor<Resu
             validateMandatoryField(collection, idBytes, "collection")
             val encryptionBlock = EncryptionBlock(keyEncryptionKeyId, initializationVector, encryptedEncryptionKey)
 
-            if (snapshotType == "drift_testing_incremental") {
+            // Note that should this should never be set to 'full', or if it is it should be
+            // strictly temporary and reverted as soon as possible. Logging the ids of all records
+            // in the full export floods cloudwatch with billions of messages which significantly
+            // slows down attempt to run queries.
+            if (snapshotType == "incremental") {
                 logger.info("Record read from hbase", "key" to printableKey(idBytes))
             }
 
