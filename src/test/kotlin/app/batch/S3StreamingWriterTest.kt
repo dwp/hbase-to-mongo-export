e3904dbd1d36f3c02d72ea2be528ea876205baf3
UdayChokkam <udayjavauk@gmail.com>
2022-02-16 11:40:21 +0000

    Shard calculation parts (#227)
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharded calculation parts
    
    * sharding quarterly
    
    * stopping manifest for full run
    
    * test fixed
    
    * test fixed
    
    * test fixed
    
    * manifest tests commented
    
    * manifest tests commented
    
    Co-authored-by: Udaykiran Chokkam <UdaykiranChokkam@digital.uc.dwp.gov.uk>

--- src/test/kotlin/app/batch/S3StreamingWriterTest.kt
+++ src/test/kotlin/app/batch/S3StreamingWriterTest.kt
@@ -35,6 +35,7 @@ import java.security.SecureRandom
     "s3.manifest.prefix.folder=manifestprefix",
     "s3.prefix.folder=prefix",
     "topic.name=db.database.collection",
+    "snapshot.type=incremental",
 ])
 class S3StreamingWriterTest {
 
